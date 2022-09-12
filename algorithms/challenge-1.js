d85764d0d7220c230d1424a0cec03e1c15425fdc
Timothy Attah <timothyattah733@gmail.com>
2022-09-12 09:51:55 +0100

    Fixed challenge-1 error

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,6 +1,6 @@
 function secondLargest(arr) {
-  const sortArr = arr.sort((a, b) => a - b);
-  return (result = sortArr[sortArr.length - 2]);
+  let sortedArray = arr.sort((a, b) => a - b);
+  return sortedArray[sortedArray.length - 2];
 }
 
 module.exports = secondLargest;
